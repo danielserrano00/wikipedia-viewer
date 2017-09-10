f10f6da053243f50a2f4dd7351c14a2848c5d4be
Daniel <danielserrano00@gmail.com>
2017-09-10 14:06:46 -0400

    Set up basic framework.

---
+++ src/client/client.js
@@ -0,0 +1,17 @@
+let searchTerm
+
+
+document.getElementById('form')
+  .addEventListener('submit', function(e){
+  e.preventDefault()
+
+    searchTerm = document.getElementsByName('search-term')[0].value
+    callApi()
+  })
+
+const baseURl =
+
+
+function callApi() {
+
+}
